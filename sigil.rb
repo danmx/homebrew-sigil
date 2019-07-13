1e40ca3e58d1057535758bd74dbc385bbb254997
danmx <daniel@iziourov.info>
2019-07-13 13:09:29 +0200

    Update to version 0.3.0, revision 257e41cb914162df9ffb2fdd1a72098deabc7eb5

--- sigil.rb
+++ sigil.rb
@@ -2,8 +2,8 @@ class Sigil < Formula
     desc "AWS SSM Session manager client"
     homepage "https://github.com/danmx/sigil"
     url "https://github.com/danmx/sigil.git",
-        :tag      => "0.2.1",
-        :revision => "f95be15e57ae9daef80f5db680e4a0c7f09e4d18"
+        :tag      => "0.3.0",
+        :revision => "257e41cb914162df9ffb2fdd1a72098deabc7eb5"
     head "https://github.com/danmx/sigil.git"
   
     # Depends on AWS session-manager-plugin
