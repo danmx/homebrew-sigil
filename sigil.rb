a2b3c4adca8ead8bf229e31cfe7c5dd80b9487fc
danmx <daniel@iziourov.info>
2020-03-21 15:51:08 +0100

    Using compiled version
    Signed-off-by: danmx <daniel@iziourov.info>

--- sigil.rb
+++ sigil.rb
@@ -1,31 +1,15 @@
 class Sigil < Formula
     desc "AWS SSM Session manager client"
     homepage "https://github.com/danmx/sigil"
-    url "https://github.com/danmx/sigil.git",
-        :tag      => "0.3.3",
-        :revision => "1826b9a8bb16816ebd89aa0fdf895b0039cfc085"
-    head "https://github.com/danmx/sigil.git"
-  
+    url "https://github.com/danmx/sigil/releases/download/0.3.3/sigil_darwin-amd64.tar.gz"
+    version "0.3.3"
+    sha256 "39fa73472d2741293369207552bf997b77d0b72584c972cf58f16b6f522503ec"
+
     # Depends on AWS session-manager-plugin
     # https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html
-
-    depends_on "go" => :build
-    depends_on "cmake" => :build
   
     def install
-        ENV["GOPATH"] = buildpath
-        dir = buildpath/"src/github.com/danmx/sigil"
-        dir.install buildpath.children - [buildpath/".brew_home"]
-        
-        cd dir do
-            ENV["VERSION"] = stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:tag]
-            ENV["REVISION"] = stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision]
-            # Bootstrap
-            system "make", "bootstrap"
-            # Make binary
-            system "make", "build-mac"
-            bin.install "bin/release/darwin/amd64/sigil"
-        end
+        bin.install "sigil"
     end
 
     def caveats; <<~EOS
@@ -38,15 +22,6 @@ class Sigil < Formula
     end
 
     test do
-        run_output = shell_output("#{bin}/sigil 2>&1")
-        assert_match "A tool for establishing a session in EC2 instances with AWS SSM Agent installed", run_output
-    
-        version_output = shell_output("#{bin}/sigil version --client 2>&1")
-        assert_match "GitTreeState:\"clean\"", version_output
-        if build.stable?
-          assert_match stable.instance_variable_get(:@resource)
-                             .instance_variable_get(:@specs)[:revision],
-                       version_output
-        end
+        system "sigil" "--version"
     end
 end
