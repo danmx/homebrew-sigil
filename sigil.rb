55a576851c962bad2b2a429d40545d48e2a9510a
danmx <daniel@iziourov.info>
2020-04-16 21:11:24 +0200

    update(release): 0.4.1
    Signed-off-by: danmx <daniel@iziourov.info>

--- sigil.rb
+++ sigil.rb
@@ -2,7 +2,7 @@ class Sigil < Formula
     desc "AWS SSM Session manager client"
     homepage "https://github.com/danmx/sigil"
     baseurl = "https://github.com/danmx/sigil/releases/download/"
-    version "0.4.0"
+    version "0.4.1"
 
     # Depends on AWS session-manager-plugin
     # https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html
@@ -11,10 +11,10 @@ class Sigil < Formula
 
     if OS.mac?
       kernel = "darwin-amd64"
-      sha256 "f343d5e87935ae46f9c081d04196079092e201a7276e77d71a18cf1e2d5f1d75"
+      sha256 "6b6d4d8cf832d82725af330863e85185acb12e35c06f9db58824eb8abf16a7ef"
     elsif OS.linux?
       kernel = "linux-amd64"
-      sha256 "19b335c1f016bad13f5840041e4f5db7953ecaf42e65ba8da7ac674a3e1ffc0a"
+      sha256 "3839144c1074b655150b933d50ede0767c534b8e284bfaaf40e43a5b7456f954"
     end
 
     url baseurl + "/" + version + "/sigil_" + kernel + ".tar.gz"
